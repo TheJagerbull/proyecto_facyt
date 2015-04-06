03903f01bcb7f7747a47a54589d429b3493f4bbd
Jose Henriquez <jahenriq@gmail.com>
2015-04-06 17:21:24 -0430

    Commit Jose Henriquez 06-04-2015

---
+++ application/modules/inv_equipos/controllers/equipos.php
@@ -0,0 +1,57 @@
+
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * 
+ * Controlador Principal Modulo Equipos
+ * =====================
+ * 
+ * @author José Henriquez en fecha: 06-04-2015
+ * 
+ */
+class Equipos extends MX_Controller
+{
+	/**
+	 * 
+	 * Clase Constructora
+	 * 
+	 * @author José Henriquez en fecha 06-04-2015
+	 * 
+	 */
+	public function __construct()
+	{
+		
+		parent::__construct();
+		
+	}
+	
+	/**
+	 * 
+	 * Muestra Pagina con mensaje Inicial y Menú
+	 * 
+	 * @author José Henriquez en fecha 06-04-2015
+	 * 
+	 */
+	public function index()
+	{
+		["titulop"]='Página de Inicio - Sistema de Inventario';
+		->load->view('home',);
+			
+	}
+
+	/**
+	 * 
+	 * Muestra Pagina con mensaje Inicial y Menú
+	 * 
+	 * @author José Henriquez en fecha 06-04-2015
+	 * 
+	 */
+	public function index()
+	{
+		["titulop"]='Página de Inicio - Sistema de Inventario';
+		->load->view('home',);
+			
+	}
+	
+	
+	
+}
