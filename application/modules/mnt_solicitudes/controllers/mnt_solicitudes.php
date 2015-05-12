98d5ced7bbd75479f693b7aacab9e770d2a832d5
root <root@ustpro-01.facyt.uc.edu.ve>
2015-05-12 17:16:01 -0430

    jcparra

--- application/modules/mnt_solicitudes/controllers/mnt_solicitudes.php
+++ application/modules/mnt_solicitudes/controllers/mnt_solicitudes.php
@@ -17,20 +17,38 @@ class Mnt_solicitudes extends MX_Controller {
     }
 
     public function lista_solicitudes($field = '', $order = '') {
-        //die_pre($this->session->all_userdata());
+
         if ($this->hasPermissionClassA()) {
-            $per_page = 6; //uso para paginacion (indica cuantas filas de la tabla, por pagina, se mostraran)
-            ///////////////////////////////////////Esta porcion de codigo, separa las URI de ordenamiento de resultados, de las URI de listado comun	
-            if (!is_numeric($this->uri->segment(3, 0))) {
-                $url = 'index.php/mnt_solicitudes/list/' . $field . '/' . $order . '/'; //uso para paginacion
-                $offset = $this->uri->segment(5, 0); //uso para consulta en BD
-                $uri_segment = 5; //uso para paginacion
-            } else {
-                $url = 'index.php/mnt_solicitudes/lista/'; //uso para paginacion
-                $offset = $this->uri->segment(3, 0); //uso para consulta en BD
-                $uri_segment = 3; //uso para paginacion
+            // $HEADER Y $VIEW SON LOS ARREGLOS DE PARAMETROS QUE SE LE PASAN A LAS VISTAS CORRESPONDIENTES
+            if ($field == 'buscar') {//control para parametros pasados a la funcion, sin esto, no se ordenan los resultados de la busqueda
+                $field = $order;
+                $order = $aux;
             }
-///////////////////////////////////////Esta porcion de codigo, separa las URI de ordenamiento de resultados, de las URI de listado comun
+            $per_page = 6; //uso para paginacion (indica cuantas filas de la tabla, por pagina, se mostraran)
+            if ($this->uri->segment(3) == 'buscar') {//para saber si la "bandera de busqueda" esta activada
+                if (!is_numeric($this->uri->segment(4, 0))) {//para saber si la "bandera de ordenamiento" esta activada
+                    $url = 'index.php/mnt_solicitudes/list/busca/' . $field . '/' . $order . '/'; //uso para paginacion
+                    $offset = $this->uri->segment(6, 0); //uso para consulta en BD
+                    $uri_segment = 6; //uso para paginacion
+                } else {
+                    $url = 'index.php/mnt_solicitudes/lista/busca/'; //uso para paginacion
+                    $offset = $this->uri->segment(4, 0); //uso para consulta en BD
+                    $uri_segment = 4; //uso para paginacion
+                }
+            } else {
+
+                $this->session->unset_userdata('query');
+                if (!is_numeric($this->uri->segment(3, 0))) {
+                    $url = 'index.php/mnt_solicitudes/list/' . $field . '/' . $order . '/'; //uso para paginacion
+                    $offset = $this->uri->segment(5, 0); //uso para consulta en BD
+                    $uri_segment = 5; //uso para paginacion
+                } else {
+                    $url = 'index.php/mnt_solicitudes/lista/'; //uso para paginacion
+                    $offset = $this->uri->segment(3, 0); //uso para consulta en BD
+                    $uri_segment = 3; //uso para paginacion
+                }
+            }
+//////////////////////////Esta porcion de codigo, separa las URI de ordenamiento de resultados, de las URI de listado comun
 
             $header['title'] = 'Ver Solicitudes';
 
@@ -38,21 +56,37 @@ class Mnt_solicitudes extends MX_Controller {
                 switch ($field) { //aqui se le "traduce" el valor, al nombre de la columna en la BD
                     case 'orden': $field = 'id_orden';
                         break;
-                    case 'tipo': $field = 'id_tipo_orden';
+                    case 'fecha': $field = 'fecha_p';
                         break;
+                    case 'responsable': $field = 'nombre';
+                        break;
+                    case 'ubicacion': $field = 'dependen';
+                        break;
+                    case 'estatus': $field = 'descripcion';
+                        break;
+                    
                     default: $field = 'id_orden';
                         break;
                     default: $field = '';
                         break; //en caso que no haya ninguna coincidencia, lo deja vacio
                 }
             }
-            $order = (empty($order) || ($order == 'desc')) ? 'asc' : 'desc';      
-            if ($_POST) {//debido a que en la vista hay un pequeno formulario para el campo de busqueda, verifico si no se le ha pasado algun valor
-                $view['mant_solicitudes'] = $this->buscar_usuario($field, $order, $per_page, $offset); //cargo la busqueda de los usuarios
-                $total_rows = $this->model_mnt_solicitudes->buscar_usrCount($_POST['usuarios']); //contabilizo la cantidad de resultados arrojados por la busqueda
+            $order = (empty($order) || ($order == 'desc')) ? 'asc' : 'desc';
+
+            if ($_POST) {
+                $this->session->set_userdata('query', $_POST['solicitudes']);
+            }
+            //die($field);
+//	     $solicitudes = $this->model_mnt_solicitudes->get_allorden($field,$order,$per_page, $offset);//el $offset y $per_page deben ser igual a los suministrados a initPagination()
+//			// PARA VER LA INFORMACION DE LOS USUARIOS DESCOMENTAR LA LINEA DE ABAJO, GUARDAR Y REFRESCAR EL EXPLORADOR
+//             die_pre($this->session->userdata('query'));
+            if ($this->uri->segment(3) == 'buscar') {//debido a que en la vista hay un pequeno formulario para el campo de busqueda, verifico si no se le ha pasado algun valor
+                // die_pre($this->session->userdata('query'));
+                $view['mant_solicitudes'] = $this->buscar_solicitud($field, $order, $per_page, $offset); //cargo la busqueda de los usuarios
+                $total_rows = $this->model_mnt_solicitudes->buscar_usrCount($this->session->userdata('query')); //contabilizo la cantidad de resultados arrojados por la busqueda
                 $config = initPagination($url, $total_rows, $per_page, $uri_segment); //inicializo la configuracion de la paginacion
                 $this->pagination->initialize($config); //inicializo la paginacion en funcion de la configuracion
-                $view['links'] = $this->pagination->create_links(); //se crean los enlaces, que solo se mostraran en la vista, si $total_rows es mayor que $per_page
+                $view['links'] = $this->pagination->create_links(); //se crean los enlaces, que solo se mostraran en la vista, si $total_rows es mayor que $per_page            
             } else {//en caso que no se haya captado ningun dato en el formulario
                 $total_rows = $this->get_alls(); //uso para paginacion
                 $view['mant_solicitudes'] = $this->model_mnt_solicitudes->get_allorden($field, $order, $per_page, $offset);
@@ -61,7 +95,7 @@ class Mnt_solicitudes extends MX_Controller {
                 $view['links'] = $this->pagination->create_links(); //NOTA, La paginacion solo se muestra cuando $total_rows > $per_page
             }
             $view['order'] = $order;
-                    
+
             // echo "Current View";
             // die_pre($view);
             //CARGAR LAS VISTAS GENERALES MAS LA VISTA DE VER USUARIO
@@ -74,14 +108,13 @@ class Mnt_solicitudes extends MX_Controller {
         }
     }
 
-    
     public function mnt_detalle($id = '') {
-       $header['title'] = 'Detalles de la Solicitud';
+        $header['title'] = 'Detalles de la Solicitud';
         if (!empty($id)) {
             $tipo = $this->model_mnt_solicitudes->get_orden($id);
             $view['tipo'] = $tipo;
 
-            //CARGAR LAS VISTAS GENERALES MAS LA VISTA DE VER ITEM
+//CARGAR LAS VISTAS GENERALES MAS LA VISTA DE VER ITEM
             $this->load->view('template/header', $header);
             if ($this->session->userdata('tipo')['id'] == $tipo->id_orden) {
                 $view['edit'] = TRUE;
@@ -100,17 +133,35 @@ class Mnt_solicitudes extends MX_Controller {
             $this->session->set_flashdata('edit_tipo', 'error');
             redirect(base_url() . 'index.php/mnt_solicitudes/detalle');
         }
-        //}
-        //else
-        //{
-        //	$header['title'] = 'Error de Acceso';
-        //	$this->load->view('template/erroracc',$header);
-        //}
+//}
+//else
+//{
+//	$header['title'] = 'Error de Acceso';
+//	$this->load->view('template/erroracc',$header);
+//}
     }
-    
-  
 
-    ////////////////////////Control de permisologia para usar las funciones
+    public function buscar_solicitud($field = '', $order = '', $per_page = '', $offset = '') {
+        die('llega');
+        if ($this->session->userdata('query')) {
+            //
+            if ($this->session->userdata('query') == '' || $this->session->userdata('query') == ' ') {
+                $this->session->unset_userdata('query');
+
+                redirect(base_url() . 'index.php/mnt_solicitudes/lista_solicitudes');
+            }
+            //die_pre($this->session->userdata('query'));
+            $header['title'] = 'Buscar Solicitudes';
+            // $post = $_POST;
+            // $request
+            return($this->model_mant_solicitudes->buscar_sol($this->session->userdata('query'), $field, $order, $per_page, $offset));
+        } else {
+            //die_pre('fin');
+            redirect('mnt_solicitudes/lista_solicitudes');
+        }
+    }
+
+////////////////////////Control de permisologia para usar las funciones
     public function hasPermissionClassA() {//Solo si es usuario autoridad y/o Asistente de autoridad
         return ($this->session->userdata('user')['sys_rol'] == 'autoridad' || $this->session->userdata('user')['sys_rol'] == 'asist_autoridad');
     }
@@ -136,4 +187,16 @@ class Mnt_solicitudes extends MX_Controller {
     }
 
     ////////////////////////Fin del Control de permisologia para usar las funciones
+    public function ajax_likeSols() {
+        //error_log("Hello", 0);
+        $solicitud = $this->input->post('solicitudes');
+        die ($solicitud);
+        header('Content-type: application/json');
+        $query = $this->model_mnt_solicitudes->ajax_likeSol($solicitud);
+        $query = objectSQL_to_array($query);
+        echo json_encode($query);
+         
+        
+    }
+
 }
--- application/modules/mnt_solicitudes/models/model_mnt_solicitudes.php
+++ application/modules/mnt_solicitudes/models/model_mnt_solicitudes.php
@@ -26,6 +26,11 @@ class Model_mnt_solicitudes extends CI_Model {
         } else {
             $this->db->order_by("id_orden", "desc");
         }
+        $query = $this->unir_todo($per_page,$offset);
+        return $query;
+    }
+ 
+    public function unir_todo($per_page= '',$offset = ''){
         $this->db->join('mnt_tipo_orden', 'mnt_tipo_orden.id_tipo = mnt_orden_trabajo.id_tipo', 'INNER');
         $this->db->join('dec_dependencia', 'dec_dependencia.id_dependencia = mnt_orden_trabajo.dependencia', 'INNER');
         $this->db->join('mnt_ubicaciones_dep', 'mnt_ubicaciones_dep.id_ubicacion = mnt_orden_trabajo.ubicacion', 'INNER');
@@ -34,27 +39,72 @@ class Model_mnt_solicitudes extends CI_Model {
         $this->db->join('mnt_estatus', 'mnt_estatus.id_estado = mnt_estatus_orden.id_estado', 'INNER');
         $this->db->join('mnt_ayudante_orden', 'mnt_ayudante_orden.id_orden_trabajo = mnt_orden_trabajo.id_orden' , 'LEFT');
         $this->db->join('dec_usuario', 'dec_usuario.id_usuario = mnt_ayudante_orden.id_responsable', 'LEFT');
-        $query = $this->db->get('mnt_orden_trabajo', $per_page, $offset);
-        return $query->result();
+        $resultado = $this->db->get('mnt_orden_trabajo', $per_page, $offset);
+        return $resultado->result();
+    }
+    
+    public function unir_unaconsulta(){
+        $this->db->join('mnt_tipo_orden', 'mnt_tipo_orden.id_tipo = mnt_orden_trabajo.id_tipo', 'INNER');
+        $this->db->join('dec_dependencia', 'dec_dependencia.id_dependencia = mnt_orden_trabajo.dependencia', 'INNER');
+        $this->db->join('mnt_ubicaciones_dep', 'mnt_ubicaciones_dep.id_ubicacion = mnt_orden_trabajo.ubicacion', 'INNER');
+        $this->db->join('mnt_observacion_orden', 'mnt_observacion_orden.id_orden_trabajo = mnt_orden_trabajo.id_orden', 'INNER');
+        $this->db->join('mnt_estatus_orden', 'mnt_estatus_orden.id_orden_trabajo = mnt_orden_trabajo.id_orden', 'INNER');
+        $this->db->join('mnt_estatus', 'mnt_estatus.id_estado = mnt_estatus_orden.id_estado', 'INNER');
+        $this->db->join('mnt_ayudante_orden', 'mnt_ayudante_orden.id_orden_trabajo = mnt_orden_trabajo.id_orden' , 'LEFT');
+        $this->db->join('dec_usuario', 'dec_usuario.id_usuario = mnt_ayudante_orden.id_responsable', 'LEFT');
+        $resultado = $this->db->get('mnt_orden_trabajo');
+        return $resultado->row();
+        
     }
 
     public function get_orden($id_orden = '') {
         if (!empty($id_orden)) {
             $this->db->where('id_orden', $id_orden);
-           // SE EXTRAEN TODOS LOS DATOS DE LA SOLICITUD
-            $this->db->order_by("id_orden", "desc");
-            $this->db->join('mnt_tipo_orden', 'mnt_tipo_orden.id_tipo = mnt_orden_trabajo.id_tipo', 'INNER');
-            $this->db->join('dec_dependencia', 'dec_dependencia.id_dependencia = mnt_orden_trabajo.dependencia', 'INNER');
-            $this->db->join('mnt_ubicaciones_dep', 'mnt_ubicaciones_dep.id_ubicacion = mnt_orden_trabajo.ubicacion', 'INNER');
-            $this->db->join('mnt_observacion_orden', 'mnt_observacion_orden.id_orden_trabajo = mnt_orden_trabajo.id_orden', 'INNER');
-            $this->db->join('mnt_estatus_orden', 'mnt_estatus_orden.id_orden_trabajo = mnt_orden_trabajo.id_orden', 'INNER');
-            $this->db->join('mnt_estatus', 'mnt_estatus.id_estado = mnt_estatus_orden.id_estado', 'INNER');
-            $this->db->join('mnt_ayudante_orden', 'mnt_ayudante_orden.id_orden_trabajo = mnt_orden_trabajo.id_orden', 'LEFT');
-            $this->db->join('dec_usuario', 'dec_usuario.id_usuario = mnt_ayudante_orden.id_responsable', 'LEFT');
-            $query = $this->db->get('mnt_orden_trabajo');
-            return $query->row();
+            $query = $this->unir_unaconsulta();
+            return $query;
         }
         return FALSE;
     }
+    
+    public function buscar_sol($usr = '', $field = '', $order = '', $per_page = '', $offset = '') {
+        die('llega');
+        if (!empty($usr)) {
+            
+            if (!empty($field)) {
+                $this->db->order_by($field, $order);
+            }
+            $usr = preg_split("/[\s,]+/", $usr);
+            $first = $usr[0];
+            if (!empty($usr[1])) {
+                $second = $usr[1];
+                $this->db->like('id_orden', $second);
+            }
+            if (!empty($usr[2])) {
+                $third = $usr[2];
+                $this->db->like('id_tipo', $third);
+            }
+            $this->db->like('id_orden', $first);
+            $this->db->or_like('id_tipo', $first);
+            $this->db->or_like('id_usuario', $first);
+            $this->db->or_like('sys_rol', $first);
+            $this->db->or_like('dependencia', $first);
+            $this->db->or_like('cargo', $first);
+            $this->db->or_like('status', $first);
+
+            return $this->db->get('mnt_orden_trabajo', $per_page, $offset)->result();
+        }
+        return FALSE;
+    }
+    
+    public function ajax_likeSol($data)
+    {        die('NADA');
+	        $this->db->like('id_orden', $data);
+//		$this->db->or_like('dependen',$data);
+//		$this->db->or_like('descripcion',$data);
+////		$query = $this->unir_todo();
+//		return $query;
+                $query = $this->db->get('mnt_orden_trabajo');
+		return $query->result();
+	}
 
 }
