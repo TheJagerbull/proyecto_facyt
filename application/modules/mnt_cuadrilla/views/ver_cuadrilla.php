101587c0599e82d2f0d8f476f85d554ab731c5c9
Juan Parra <jcparra@uc.edu.ve>
2015-12-01 09:15:40 -0430

    Modificaciones en ver cuadrilla

--- application/modules/mnt_cuadrilla/views/ver_cuadrilla.php
+++ application/modules/mnt_cuadrilla/views/ver_cuadrilla.php
@@ -6,32 +6,28 @@
     var table;
     var save_method;
     var tabla;
-    var rows_selected = [];
+    var rows_selected = []; // Array donde se guardan los id de las columnas seleccionadas
     $(document).ready(function () {
        tabla = $('#trabajadores').DataTable({ 
         responsive: true,
         "processing": true, //Feature control the processing indicator.
         "serverSide": true, //Feature control DataTables' server-side processing mode.
-//         "ordering": false,
-         "searching": false,
-         'order': [[0, 'asc']],
-//         "bLengthChange": false,
-         "iDisplayLength": 5,
-         'sDom': 'tp',
-        // Load data for the table's content from an Ajax source
+        "searching": false,
+        "order": [[0, 'asc']],
+        "iDisplayLength": 5,
+        "sDom": 'tp',
+        // Leer los datos desde la fuente para llenar la tabla por ajax
         "ajax": {
             "url": "<?php echo site_url('mnt_cuadrilla/cuadrilla/ajax_detalle/'.$item['id'])?>",
             "type": "POST"
         },
-    
-        //Set column definition initialisation properties.
-       
+        //Para configurar la ultima columna.
         "columnDefs": [
         { 
-           "className": 'dt-head-center',
-          "targets": [2], //last column
-          "orderable": false, //set not orderable
-          "searchable": false
+          "className": 'dt-head-center',
+          "targets": [2], //ultima columna
+          "orderable": false, //para que no sea ordenable
+          "searchable": false //deshabilita la busqueda
         }
         ]
 
@@ -39,153 +35,120 @@
 
 });  
 function add_trabajador()
-      {
+{
        save_method = 'add';
-       $('#modificar').modal('show'); // show bootstrap modal
-       $('#modifica')[0].reset(); // reset form on modals
-//       var table = $('#trabajadores2').DataTable({
-//            "ajax":"<?php echo base_url('index.php/mnt_cuadrilla/cuadrilla/mostrar_unassigned/'.$item['id']); ?>",
-//            "bLengthChange": false,
-//             "aoColumnDefs": [{
-//                "orderable": false,
-//                'searchable':false,
-//                 "targets": [0],
-//                 'render': function (data, type, full, meta){
-//             return '<input type="checkbox" class="glyphicon glyphicon-minus" name="id_ayudantes[]" value="' + $('<div/>').text(data).html() + '">';}
-//             }],
-//             'order': [[1, 'asc']],
-//            "iDisplayLength": 5,
-//            destroy: true
-//          });
-  //
-// Updates "Select all" control in a data table
-//
-function updateDataTableSelectAllCtrl(table){
-   var $table             = table.table().node();
-   var $chkbox_all        = $('tbody input[type="checkbox"]', $table);
-   var $chkbox_checked    = $('tbody input[type="checkbox"]:checked', $table);
-   var chkbox_select_all  = $('thead input[name="select_all"]', $table).get(0);
+       $('#modificar').modal('show'); // muestra bootstrap modal
+       $('#modifica')[0].reset(); // para aplicar reset a los form en modals
 
-   // If none of the checkboxes are checked
-   if($chkbox_checked.length === 0){
-      chkbox_select_all.checked = false;
-      if('indeterminate' in chkbox_select_all){
-         chkbox_select_all.indeterminate = false;
-      }
+// para actualizar el checkbox "Select all" control en el data table
+    function updateDataTableSelectAllCtrl(table){
+        var $table             = table.table().node();
+        var $chkbox_all        = $('tbody input[type="checkbox"]', $table);
+        var $chkbox_checked    = $('tbody input[type="checkbox"]:checked', $table);
+        var chkbox_select_all = $('thead input[name="select_all"]', $table).get(0);
 
-   // If all of the checkboxes are checked
-   } else if ($chkbox_checked.length === $chkbox_all.length){
-      chkbox_select_all.checked = true;
-      if('indeterminate' in chkbox_select_all){
-         chkbox_select_all.indeterminate = false;
-      }
+        // Si no hay checkboxes seleccionados 
+            if ($chkbox_checked.length === 0) {
+                chkbox_select_all.checked = false;
+                if ('indeterminate' in chkbox_select_all) {
+                    chkbox_select_all.indeterminate = false;
+                }
 
-   // If some of the checkboxes are checked
-   } else {
-      chkbox_select_all.checked = true;
-      if('indeterminate' in chkbox_select_all){
-         chkbox_select_all.indeterminate = true;
-      }
-   }
-}
+        // Si todos los checkboxes son seleccionados
+            } else if ($chkbox_checked.length === $chkbox_all.length) {
+                chkbox_select_all.checked = true;
+                if ('indeterminate' in chkbox_select_all) {
+                    chkbox_select_all.indeterminate = false;
+                }
 
-$(document).ready(function (){
-   // Array holding selected row IDs
-   
-    table = $('#trabajadores2').DataTable({
-        responsive: true,
-       "ajax": "<?php echo base_url('index.php/mnt_cuadrilla/cuadrilla/mostrar_unassigned/'.$item['id']); ?>",
-       "bLengthChange": false,
-             "aoColumnDefs": [{
+        // Si algin checkboxes es seleccionado
+            } else {
+                chkbox_select_all.checked = true;
+                if ('indeterminate' in chkbox_select_all) {
+                    chkbox_select_all.indeterminate = true;
+                }
+            }
+    }
+
+    $(document).ready(function (){
+        table = $('#trabajadores2').DataTable({
+            responsive: true,
+            "ajax": "<?php echo base_url('index.php/mnt_cuadrilla/cuadrilla/mostrar_unassigned/' . $item['id']); ?>",
+            "bLengthChange": false,
+            "aoColumnDefs": [{
                 "orderable": false,
-                'searchable':false,
+                "searchable": false,
                 "className": 'dt-head-center',
-                 "targets": [0],
-                 
-                 'render': function (data, type, full, meta){
-             return '<input type="checkbox" class="icon-checkbox" value="' + $('<div/>').text(data).html() + '"><label for="checkbox1"><span style="color:#D9534F" id="clickable" class="glyphicon glyphicon-minus unchecked"></span><span id="clickable" class="glyphicon glyphicon-plus checked color"></span></label>';
-              }
-             }],
-             "order": [[1, 'asc']],
+                "targets": [0],
+                'render': function (data, type, full, meta) {  //genera un checkbox en toda la columna 0 de la tabla
+                    return '<input type="checkbox" class="icon-checkbox" value="' + $('<div/>').text(data).html() + '"><label for="checkbox1"><span style="color:#D9534F" id="clickable" class="glyphicon glyphicon-minus unchecked"></span><span id="clickable" class="glyphicon glyphicon-plus checked color"></span></label>';
+                }
+            }],
+            "order": [[1, 'asc']],
             "iDisplayLength": 5,
             destroy: true,
-      'rowCallback': function(row, data, dataIndex){
-         // Get row ID
-         var rowId = data[0];
+            'rowCallback': function (row, data, dataIndex) {
+                // Para obtener el ID de la columna 
+                var rowId = data[0];
+                // Si el ID de la columna esta en la lista de los id de la columna seleccionada
+                if ($.inArray(rowId, rows_selected) !== -1) {
+                    $(row).find('input[type="checkbox"]').prop('checked', true);
+                    $(row).addClass('selected');
+                }
+            }
+        });
+        table.columns.adjust();
+        // Handle click on checkbox
+        $('#trabajadores2 tbody').on('click', 'input[type="checkbox"]', function (e) {
+            var $row = $(this).closest('tr');
+            // Get row data
+            var data = table.row($row).data();
+            // Get row ID
+            var rowId = data[0];
+            // Determine whether row ID is in the list of selected row IDs 
+            var index = $.inArray(rowId, rows_selected);
+            // If checkbox is checked and row ID is not in list of selected row IDs
+            if (this.checked && index === -1) {
+                rows_selected.push(rowId);
+               // Otherwise, if checkbox is not checked and row ID is in list of selected row IDs
+            } else if (!this.checked && index !== -1) {
+                rows_selected.splice(index, 1);
+            }
+            if (this.checked) {
+                $row.addClass('selected');
+            } else {
+                $row.removeClass('selected');
+            }
+            // Update state of "Select all" control
+            updateDataTableSelectAllCtrl(table);
+            // Prevent click event from propagating to parent
+            e.stopPropagation();
+        });
+        // Handle click on table cells with checkboxes
+        $('#trabajadores2').on('click', 'tbody td, thead th:first-child', function (e) {
+            $(this).parent().find('input[type="checkbox"]').trigger('click');
+        });
+        // Handle click on "Select all" control
+        $('#trabajadores2 thead input[name="select_all"]').on('click', function (e) {
+            if (this.checked) {
+                $('#trabajadores2 tbody input[type="checkbox"]:not(:checked)').trigger('click');
+            } else {
+                $('#trabajadores2 tbody input[type="checkbox"]:checked').trigger('click');
+            }
+            // Prevent click event from propagating to parent
+            e.stopPropagation();
+        });
+        // Handle table draw event
+        table.on('draw', function () {
+            // Update state of "Select all" control
+            updateDataTableSelectAllCtrl(table);
+        });
+    });
 
-         // If row ID is in the list of selected row IDs
-         if($.inArray(rowId, rows_selected) !== -1){
-            $(row).find('input[type="checkbox"]').prop('checked', true);
-            $(row).addClass('selected');
-         }
-      }
-   });
-   table.columns.adjust();
-   // Handle click on checkbox
-   $('#trabajadores2 tbody').on('click', 'input[type="checkbox"]', function(e){
-      var $row = $(this).closest('tr');
+}
 
-      // Get row data
-      var data = table.row($row).data();
-
-      // Get row ID
-      var rowId = data[0];
-
-      // Determine whether row ID is in the list of selected row IDs 
-      var index = $.inArray(rowId, rows_selected);
-
-      // If checkbox is checked and row ID is not in list of selected row IDs
-      if(this.checked && index === -1){
-         rows_selected.push(rowId);
-
-      // Otherwise, if checkbox is not checked and row ID is in list of selected row IDs
-      } else if (!this.checked && index !== -1){
-         rows_selected.splice(index, 1);
-      }
-
-      if(this.checked){
-         $row.addClass('selected');
-      } else {
-         $row.removeClass('selected');
-      }
-
-      // Update state of "Select all" control
-      updateDataTableSelectAllCtrl(table);
-
-      // Prevent click event from propagating to parent
-      e.stopPropagation();
-   });
-
-   // Handle click on table cells with checkboxes
-   $('#trabajadores2').on('click', 'tbody td, thead th:first-child', function(e){
-      $(this).parent().find('input[type="checkbox"]').trigger('click');
-   });
-
-   // Handle click on "Select all" control
-   $('#trabajadores2 thead input[name="select_all"]').on('click', function(e){
-      if(this.checked){
-         $('#trabajadores2 tbody input[type="checkbox"]:not(:checked)').trigger('click');
-      } else {
-         $('#trabajadores2 tbody input[type="checkbox"]:checked').trigger('click');
-      }
-
-      // Prevent click event from propagating to parent
-      e.stopPropagation();
-   });
-
-   // Handle table draw event
-   table.on('draw', function(){
-      // Update state of "Select all" control
-      updateDataTableSelectAllCtrl(table);
-   });
-    
-
-});
-
-       }
-
-    function delete_person(id)
-    {
+function delete_person(id)
+{
     swal({
         title: "",
         text: "¿Seguro que desea eliminar este registro?'",
@@ -194,104 +157,96 @@ $(document).ready(function (){
         confirmButtonClass: "btn-danger",
         confirmButtonText: "Si",
         closeOnConfirm: false
-     },
-     function()
-//      if(confirm('¿Seguro que desea eliminar este registro?'))
-      {
-         var cuad = "<?php echo $item['id']?>";
-         var dataString = 'id='+ id + '&cuad='+ cuad;
+    },
+    function()
+    {
+        var cuad = "<?php echo $item['id']?>";
+        var dataString = 'id='+ id + '&cuad='+ cuad;
         // ajax delete data to database
-          $.ajax({
+        $.ajax({
             url : "<?php echo site_url('mnt_cuadrilla/cuadrilla/ajax_borrar')?>/"+id,
             type: "POST",
             data: dataString,
             dataType: "JSON",
             success: function(data)
             {
-//               console.log(data);
-               if (data.status){
-                 swal(" ","El trabajador ha sido removido de la cuadrilla", "success");
-                 reload_table(); 
-               }else{
-                 swal(" ","Este trabajador es el responsable de la cuadrilla, por lo tanto no lo puede borrar ", "error"); 
+//              console.log(data);
+                if (data.status){
+                    swal(" ","El trabajador ha sido removido de la cuadrilla", "success");
+                    reload_table(); 
+                }else{
+                    swal(" ","Este trabajador es el responsable de la cuadrilla, por lo tanto no lo puede borrar ", "error"); 
+                }
             }
-           }
         });
         
-      });
-    }
+    });
+}
 
-       function reload_table()
-    {
-      tabla.ajax.reload(null,false); //reload datatable ajax 
-    }
-     
-   function guardar()
-    {
-      var url;
-      if(save_method === 'add') 
-      {
-          url = "<?php echo site_url('mnt_cuadrilla/cuadrilla/ajax_guardar/'.$item['id'])?>";
-      }
-         // Handle form submission event 
-   $('#modifica').on('submit', function(e){
-      var form = this;
+function reload_table()
+{
+    tabla.ajax.reload(null,false); //reload datatable ajax 
+}
 
-      // Iterate over all selected checkboxes
-      $.each(rows_selected, function(index, rowId){
-         // Create a hidden element 
-         $(form).append(
-             $('<input>')
-                .attr('type', 'hidden')
-                .attr('name', 'id_ayudantes[]')
-                .val(rowId)
-         );
-      });
-             $.ajax({
+function guardar()
+{
+    var url;
+    if(save_method === 'add') 
+    {
+        url = "<?php echo site_url('mnt_cuadrilla/cuadrilla/ajax_guardar/'.$item['id'])?>";
+    }
+    // Handle form submission event 
+    $('#modifica').on('submit', function(e){
+        var form = this;
+        // Iterate over all selected checkboxes
+        $.each(rows_selected, function(index, rowId){
+        // Create a hidden element 
+            $(form).append(
+                $('<input>')
+                    .attr('type', 'hidden')
+                    .attr('name', 'id_ayudantes[]')
+                    .val(rowId)
+                );
+        });
+        $.ajax({
             url : url,
             type: "POST",
             data: ($(form).serialize()),
             dataType: "JSON",
             success: function(data)
             {
-               //if success close modal and reload ajax table
-//               $(this).closest('form').get(0).reset();
-               $('#modificar').modal('hide');
-               
-               reload_table();
+                //if success close modal and reload ajax table
+//              $(this).closest('form').get(0).reset();
+                $('#modificar').modal('hide');
+                reload_table();
             },
             error: function (jqXHR, textStatus, errorThrown)
             {
                 swal(" ","Error agregando datos, por favor verifique y vuelva a intentarlo ", "error");
-//                alert('Error agregando los datos');
             }
         });
-      // FOR DEMONSTRATION ONLY     
+            // FOR DEMONSTRATION ONLY     
       
-//       Output form data to a console     
+//      Output form data to a console     
 //      $('#trabajadores2-console').text($(form).serialize());
 //      console.log("Form submission", $(form).serialize());
        
-      // Remove added elements
-      $('input[name="id_ayudantes\[\]"]', form).remove();
-       (form).get(0).reset();
-      // Prevent actual form submission
-      
-      // ajax adding data to database
-      
+        // Remove added elements
+        $('input[name="id_ayudantes\[\]"]', form).remove();
+        (form).get(0).reset();
+        // Prevent actual form submission
+        // ajax adding data to database
         e.preventDefault();
-   });
-       
-    }
+    });
+}
      
 function edit_var(id)
 {
     save_method === 'update';
 //    $('#form')[0].reset(); // reset form on modals
 //    $('.form-group').removeClass('has-error'); // clear error class
-    $('.help-block').empty(); // clear error string
    
-    //Ajax Load data from ajax
+    //Ajax Carga de datos desde ajax
     $.ajax({
         url : "<?php echo site_url('mnt_cuadrilla/cuadrilla/ajax_edit/')?>/" + id,
         type: "POST",
@@ -303,25 +258,23 @@ function edit_var(id)
             var jsonObject = eval(data.obreros);
             for (var n = 0; n < jsonObject.length; n++) {
               selectObject[0].options[n] = new Option(jsonObject[n].nombre +' '+ jsonObject[n].apellido + ' '+'Cargo: '+ jsonObject[n].cargo,jsonObject[n].id_usuario);
-              };
-             $('[name="id_trabajador_responsable"]').select2({theme: "bootstrap"}).select2("val",data.id_trabajador_responsable);
-             $("#file-3").fileinput({
-            url: (base_url + 'index.php/mnt_cuadrilla/cuadrilla/crear_cuadrilla'),
-            showUpload: false,
-            'initialPreview': "<img style='height:160px' src= '<?php echo base_url()?>"+data.icono+"' class='file-preview-image'>",
-            language: 'es',
-            showCaption: false,
-            browseClass: "btn btn-primary btn-sm",
-            allowedFileExtensions: ['png'],
-            maxImageWidth: 512,
-            maxImageHeight: 512
-        });
+            };
+            $('[name="id_trabajador_responsable"]').select2({theme: "bootstrap"}).select2("val",data.id_trabajador_responsable);
+            $("#file-3").fileinput({
+                url: (base_url + 'index.php/mnt_cuadrilla/cuadrilla/crear_cuadrilla'),
+                showUpload: false,
+                'initialPreview': "<img style='height:160px' src= '<?php echo base_url()?>"+data.icono+"' class='file-preview-image'>",
+                language: 'es',
+                showCaption: false,
+                browseClass: "btn btn-primary btn-sm",
+                allowedFileExtensions: ['png'],
+                maxImageWidth: 512,
+                maxImageHeight: 512
+            });
             var nomb = data.icono.split('/');
             var nomb_fin = nomb[3].split('.');
             $('#nombre_img').val(nomb_fin[0]);
-            $('#editar').modal('show'); // show bootstrap modal when complete loaded
-           
- 
+            $('#editar').modal('show'); // show bootstrap modal when complete loaded         
         },
         error: function (jqXHR, textStatus, errorThrown)
         {
