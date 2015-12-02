230a36de283b69d1f9f17cd085a41ef3c4f48707
Ilse Moreno <inatalymoreno@gmail.com>
2015-12-02 12:45:14 -0430

    cambios en la pagina de inicio

--- application/modules/user/views/start.php
+++ application/modules/user/views/start.php
@@ -1,12 +1,29 @@
 <div class="mainy">
 	<div class="awidget-body">
-		<div class="page-title text-center">
-			<h1>Bienvenido</h1>
-			<hr />
-			<div class="element">
-	            <img src="<?php echo base_url() ?>assets/img/principal.jpg" alt=""/>
-	        </div>
+		<div class="page-header">
+    <h1><strong>SiSAI</strong></h1>
+		</div>
+			<div class="row">
+				<div class="element">
+		            <img  align ="left" src="" alt=""/>
+		        </div>
+				<div class="element">
+		            <img  align ="right" src="<?php echo base_url() ?>assets/img/principal1.jpg" alt=""/>
+		        </div>
+		        <br><br>
+		        <div class="element">
+		            <img  align ="right" src="<?php echo base_url() ?>assets/img/facyt-mediano2.png" alt=""/>
+		        </div>
+		    </div>
 	        <b hidden><?php echo CI_VERSION;?></b>
+		
+		
+		<div class="container-fluid bg-3 text-left">
+		        <div class="col-sm-4">
+		        	<div class="well">
+		        		<h4><strong>UST</strong><br>Desarrollo y Programación<br>0241-6004000 Ext. 315191</h4> 
+		        	</div>
+		        </div>
 		</div>
 	</div>
 </div>
