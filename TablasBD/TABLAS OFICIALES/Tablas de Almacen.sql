a4479a54ef687b3d72f2c00010e8adf0b3acee02
luigi_palacios <luigiepa87@gmail.com>
2016-03-29 15:15:21 -0430

    inicio de modificaciones sobre las tablas de la base de datos

--- TablasBD/TABLAS OFICIALES/Tablas de Almacen.sql
+++ TablasBD/TABLAS OFICIALES/Tablas de Almacen.sql
@@ -1,15 +1,76 @@
 --
 -- Tablas de Almacen, Inventario y Solicitudes de almacen
 --
-CREATE TABLE IF NOT EXISTS `alm_aprueba` (
+CREATE TABLE IF NOT EXISTS `alm_historial_s` (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `ref_historial` varchar(10) NOT NULL,
+  `fecha_gen` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `fecha_ap` timestamp NULL DEFAULT NULL,
+  `fecha_desp` timestamp NULL DEFAULT NULL,
+  `fecha_comp` timestamp NULL DEFAULT NULL,
+  `usuario_gen` varchar(9) NOT NULL,
+  `usuario_ap` varchar(9) DEFAULT NULL,,
+  `usuario_desp` varchar(9) DEFAULT NULL,,
+  `usuario_comp` varchar(9) DEFAULT NULL,
+  PRIMARY KEY (`ref_historial`),
+  UNIQUE KEY `ID` (`ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `alm_cambia_estado` (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id_usuario` varchar(9) NOT NULL,
+  `nr_solicitud` varchar(9) NOT NULL,
+  `ref_historial` varchar(10) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `id_usuario` (`id_usuario`,`nr_solicitud`),
+  UNIQUE KEY `ID` (`ID`),
+  KEY `nr_solicitud` (`nr_solicitud`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `alm_genera` (
   `ID` bigint(20) NOT NULL AUTO_INCREMENT,
   `TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `id_usuario` varchar(9) NOT NULL,
   `nr_solicitud` varchar(9) NOT NULL,
   PRIMARY KEY (`ID`),
-  UNIQUE KEY `id_usuario` (`id_usuario`,`nr_solicitud`),
-  UNIQUE KEY `ID` (`ID`),
+  UNIQUE KEY `genera` (`id_usuario`,`nr_solicitud`),
   KEY `nr_solicitud` (`nr_solicitud`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `alm_edita` (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id_usuario` varchar(9) NOT NULL,
+  `id_estado_articulo` varchar(19) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `edita` (`id_usuario`,`nr_solicitud`),
+  KEY `nr_solicitud` (`nr_solicitud`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `alm_estado_articulo` (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id_usuario` varchar(9) NOT NULL,
+  `id_estado_articulo` varchar(19) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `edita` (`id_usuario`,`nr_solicitud`),
+  KEY `nr_solicitud` (`nr_solicitud`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `alm_retira` (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `nr_solicitud` varchar(9) NOT NULL,
+  `cod_articulo` varchar(9) NOT NULL,
+  `id_usuario` varchar(9) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID` (`ID`),
+  UNIQUE KEY `traslado_articulo` (`nr_solicitud`,`cod_articulo`,`id_usuario`),
+  KEY `cod_articulo` (`cod_articulo`),
+  KEY `id_usuario` (`id_usuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `alm_articulo` (
@@ -57,25 +118,15 @@ CREATE TABLE IF NOT EXISTS `alm_contiene` (
   `ID` bigint(20) NOT NULL AUTO_INCREMENT,
   `TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `id_articulo` bigint(20) NOT NULL,
-  `NRS` varchar(9) NOT NULL,
+  `id_estado_articulo` varchar(19) NOT NULL,
   `nr_solicitud` varchar(9) NOT NULL,
   `cant_solicitada` int(11) NOT NULL,
   `cant_aprobada` int(11) DEFAULT NULL,
   `cant_usados` int(11) DEFAULT '0',
   `cant_nuevos` int(11) DEFAULT '0',
   UNIQUE KEY `ID` (`ID`),
-  UNIQUE KEY `cont_histo_solicitud` (`id_articulo`,`nr_solicitud`,`NRS`),
-  KEY `NRS` (`NRS`),
-  KEY `nr_solicitud` (`nr_solicitud`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
-CREATE TABLE IF NOT EXISTS `alm_genera` (
-  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
-  `TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `id_usuario` varchar(9) NOT NULL,
-  `nr_solicitud` varchar(9) NOT NULL,
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `genera` (`id_usuario`,`nr_solicitud`),
+  UNIQUE KEY `cont_histo_solicitud` (`id_articulo`,`nr_solicitud`,`id_estado_articulo`),
+  KEY `id_estado_articulo` (`id_estado_articulo`),
   KEY `nr_solicitud` (`nr_solicitud`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -103,20 +154,6 @@ CREATE TABLE IF NOT EXISTS `alm_historial_a` (
   UNIQUE KEY `ID` (`ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
-CREATE TABLE IF NOT EXISTS `alm_historial_s` (
-  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
-  `TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `NRS` varchar(10) NOT NULL,
-  `fecha_gen` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `fecha_ap` timestamp NULL DEFAULT NULL,
-  `fecha_desp` timestamp NULL DEFAULT NULL,
-  `fecha_comp` timestamp NULL DEFAULT NULL,
-  `usuario_gen` varchar(9) NOT NULL,
-  `usuario_ap` varchar(9) DEFAULT NULL,
-  PRIMARY KEY (`NRS`),
-  UNIQUE KEY `ID` (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
 --
 CREATE TABLE IF NOT EXISTS `alm_pertenece` (
   `ID` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -129,25 +166,12 @@ CREATE TABLE IF NOT EXISTS `alm_pertenece` (
   UNIQUE KEY `cod_cartegoria` (`cod_cartegoria`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
-CREATE TABLE IF NOT EXISTS `alm_retira` (
-  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
-  `TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `nr_solicitud` varchar(9) NOT NULL,
-  `cod_articulo` varchar(9) NOT NULL,
-  `id_usuario` varchar(9) NOT NULL,
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `ID` (`ID`),
-  UNIQUE KEY `traslado_articulo` (`nr_solicitud`,`cod_articulo`,`id_usuario`),
-  KEY `cod_articulo` (`cod_articulo`),
-  KEY `id_usuario` (`id_usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
 CREATE TABLE IF NOT EXISTS `alm_solicitud` (
   `ID` bigint(20) NOT NULL AUTO_INCREMENT,
   `TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `id_usuario` varchar(9) NOT NULL,
   `nr_solicitud` varchar(9) NOT NULL,
-  `status` enum('carrito','en_proceso','aprobada','enviado','completado') NOT NULL,
+  `status` enum('carrito','en_proceso','aprobado','enviado','completado', 'cancelado', 'anulado', 'cerrado') NOT NULL,
   `observacion` text,
   `fecha_gen` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `fecha_comp` timestamp NULL DEFAULT NULL,
@@ -159,7 +183,7 @@ CREATE TABLE IF NOT EXISTS `alm_solicitud` (
 --
 -- Constraints for table `alm_aprueba`
 --
-ALTER TABLE `alm_aprueba`
+ALTER TABLE `alm_cambia_estado`
   ADD CONSTRAINT `alm_aprueba_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `dec_usuario` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `alm_aprueba_ibfk_2` FOREIGN KEY (`nr_solicitud`) REFERENCES `alm_solicitud` (`nr_solicitud`) ON DELETE CASCADE ON UPDATE CASCADE;
 
@@ -175,14 +199,14 @@ ALTER TABLE `alm_consulta`
 --
 ALTER TABLE `alm_contiene`
   ADD CONSTRAINT `alm_contiene_ibfk_2` FOREIGN KEY (`nr_solicitud`) REFERENCES `alm_solicitud` (`nr_solicitud`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `alm_contiene_ibfk_3` FOREIGN KEY (`NRS`) REFERENCES `alm_historial_s` (`NRS`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `alm_contiene_ibfk_3` FOREIGN KEY (`id_estado_articulo`) REFERENCES `alm_estado_articulo` (`id_estado_articulo`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `alm_contiene_ibfk_4` FOREIGN KEY (`id_articulo`) REFERENCES `alm_articulo` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `alm_genera`
 --
 ALTER TABLE `alm_genera`
-  ADD CONSTRAINT `alm_genera_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `alm_solicitud` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `alm_genera_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `dec_usuario` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `alm_genera_ibfk_2` FOREIGN KEY (`nr_solicitud`) REFERENCES `alm_solicitud` (`nr_solicitud`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
