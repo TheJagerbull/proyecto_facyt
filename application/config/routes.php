28fe92b2a05561d46859be37bfc8de577e50667b
Juan Parra <jcparra@uc.edu.ve>
2016-06-02 10:46:48 -0400

    Modificacion de la ruta para salir del mantenimiento del sistema

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,7 +38,7 @@
 |
 */
 
-$route['default_controller'] 									='template/under_construction';//= "user/usuario";
+$route['default_controller'] 									= "user/usuario";//'template/under_construction';
 $route['404_override'] 											= '';
 $route['test']													= 'template/template';
 //inicio
