612a0161eff2e0a6b465cb948a643e3c941762a1
Juan Parra <jcparra@uc.edu.ve>
2017-02-10 16:13:24 -0400

    Pruebas de fuera de linea

--- application/config/application.php
+++ application/config/application.php
@@ -8,8 +8,9 @@
 | $config['is_offline'] = TRUE; // sitio offline
 | $config['is_offline'] = FALSE; // sitio online
 | $config['offline_allowed_ips'] = array('xxx.xxx.xxx.xxx') // ips permitidos
+ * 127.0.0.1 local host
 */
  
-$config['is_offline'] = FALSE;
-$config['offline_allowed_ips'] = array('127.0.0.1');
+$config['is_offline'] = TRUE;
+$config['offline_allowed_ips'] = array('190.170.87.3');
 
